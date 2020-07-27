b3e6bcf6976ac0a7424b5270981636031af7ba4c
Hussein Talal <hossientalal2@gmail.com>
2020-07-27 17:40:11 +0300

    adding footer

---
+++ src/containers/Footer/Footer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+function Footer() {
+  return (
+    <footer className="text-white">
+      <Container className="h-100">
+        <Row className="h-100 align-content-center ">
+          <Col className="mb-3 first-col" md="3">
+            <h5>Doctor Booking</h5>
+            <p>A website to book doctor appointment and save you the time</p>
+          </Col>
+          <Col md="3" className="mb-3 offset-md-2">
+            <h5>Links</h5>
+
+            <ul className="list-unstyled">
+              <li>
+                <a href="#!">Home</a>
+              </li>
+              <li>
+                <a href="#!">About</a>
+              </li>
+              <li>
+                <a href="#!">FAQ</a>
+              </li>
+            </ul>
+          </Col>
+
+          <Col md="3" className="mb-3 offset-md-1">
+            <h5>Services</h5>
+
+            <ul className="list-unstyled">
+              <li>
+                <a href="#!">Book an appointment</a>
+              </li>
+              <li>
+                <a href="#!">Clinic registration</a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
+
+export default Footer;
