8562f679bbd482f4f4d1ec019e68ac938c0fd031
Hussein Talal <hossientalal2@gmail.com>
2020-07-27 17:41:28 +0300

    fixing eslint problems

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/style.css'
+import './styles/style.css';
 
 ReactDOM.render(
   <React.StrictMode>
--- src/tests/Navbar.test.js
+++ src/tests/Navbar.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Navbar from "../containers/Navbar/Navbar";
-import renderer from "react-test-renderer"
-test("navbar is rendering ", ()=> {
-    render(<Navbar />)
-})
+import Navbar from '../containers/Navbar/Navbar';
+import renderer from 'react-test-renderer';
+test('navbar is rendering ', () => {
+  render(<Navbar />);
+});
 
-test("navbar renders correctly", ()=> {
-    const tree = renderer.create(<Navbar />).toJSON();
-    expect(tree).toMatchSnapshot();
-})
+test('navbar renders correctly', () => {
+  const tree = renderer.create(<Navbar />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
