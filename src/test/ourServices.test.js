69d6d129085f63fe97eba663ccb2a25d50822aac
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-07-28 16:21:34 +0300

    failed test

--- src/test/ourServices.test.js
+++ src/test/ourServices.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
-
-
-test('renders', () => {
-  expect(true).toBeTruthy();
+import renderer from 'react-test-renderer';
+import OurServices from '..//Containers/OurServices/OurServices';
+import { Container } from 'react-bootstrap';
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Container/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
