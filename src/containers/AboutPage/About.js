7a788cf24d4dc095bcd8fc395ecc0e5d1bda8f46
Hussein Talal <hossientalal2@gmail.com>
2020-07-28 10:56:04 +0300

    init about page and about section

---
+++ src/containers/AboutPage/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AboutSection from '../../components/AboutSection';
+function About() {
+  return (
+    <>
+      <AboutSection />
+    </>
+  );
+}
+
+export default About;
