b1edfe1e2d77fc0035e53c50c107f6255fdccd90
Hussein Talal <hossientalal2@gmail.com>
2020-07-29 00:35:16 +0300

    fixing lint problems

--- src/tests/AboutSection.test.js
+++ src/tests/AboutSection.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import AboutSection from '../components/AboutSection/AboutSection';
 import renderer from 'react-test-renderer';
-import About from '../containers/AboutPage/About';
 
 test('About section renders', () => {
   render(<AboutSection />);
