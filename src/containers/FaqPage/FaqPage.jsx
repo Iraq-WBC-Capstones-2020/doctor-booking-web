a9a66675544d2d63eb940f76211d6a34a6a4f47b
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-07-28 15:24:18 +0300

    finish intro section in FaqPage

---
+++ src/containers/FaqPage/FaqPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FaqIntro from '../../components/FaqIntro/FaqIntro';
+
+function FaqPage() {
+  return (
+    <div>
+      <FaqIntro />
+    </div>
+  );
+}
+
+export default FaqPage;
