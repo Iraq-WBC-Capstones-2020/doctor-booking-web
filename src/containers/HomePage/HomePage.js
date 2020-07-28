8a814be61eca6e8cdbbdebcf31d989c221b2a03b
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-07-28 14:29:13 +0300

    ]

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import IntroSection from '../../components/IntroSection/IntroSection';
+import GetInTouch from '../../components/GetInTouch/GetInTouch';
 
 function HomePage() {
   return (
     <div>
       <IntroSection />
+      <GetInTouch />
     </div>
   );
 }
