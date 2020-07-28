9bff503d4abd9b1185d30a244049b756d0ae5164
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-07-28 14:08:47 +0300

    adding folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HomePage from './containers/HomePage/HomePage';
 
 function App() {
   return (
     <div>
-      <h1>Doctor Booking</h1>
+      <Router>
+        <Switch>
+          <Route path="/" component={HomePage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/containers/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <IntroSection />
+    </div>
+  );
+}
+
+export default HomePage;
