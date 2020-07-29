700483ab39f092b87f110c5e83c6b9a8645c903a
Hussein Talal <hossientalal2@gmail.com>
2020-07-29 11:54:50 +0300

    init our goals section

---
+++ src/components/OurGoals/OurGoals.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+function OurGoals() {
+  return (
+    <Container>
+      <Row>
+        <Col md="4"></Col>
+        <Col md="4"></Col>
+        <Col md="4"></Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default OurGoals;
