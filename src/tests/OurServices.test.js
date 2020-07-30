880be0fc9b2c8c3cea2f59450599d2002df066bb
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-07-30 14:32:09 +0300

    run the tests

--- src/tests/OurServices.test.js
+++ src/tests/OurServices.test.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import OurServices from '../components/OurServices/OurServices'
 import renderer from 'react-test-renderer';
-test('OurServices is rendering ', () => {
-  render(<OurServices />);
-});
-
-test('OurServices renders correctly', () => {
-  const tree = renderer.create(<OurServices />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+it('render1', () => {
+    render(<OurServices />);
+  });
+  
+  it('renders correctly', () => {
+    const tree = renderer.create(<OurServices />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
