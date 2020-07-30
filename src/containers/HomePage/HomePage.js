449305e2db21ef0882d39e4f8b1e07893e0658aa
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-07-30 14:41:22 +0300

    yarn run lint --fix

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import OurServices from '../../components/OurServices/OurServices'
+import React from 'react';
+import OurServices from '../../components/OurServices/OurServices';
 
-function HomePage(){
-
-    return(
-        <>
-        <OurServices />
-        </>
-    )
-
+function HomePage() {
+  return (
+    <>
+      <OurServices />
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
