2eb52201e66ab21691aea4514bd4ec67f9f760bb
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-07-30 16:04:52 +0300

    created the home page

---
+++ src/containers/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import AppSection from '../../components/AppSection/AppSection'
+
+function HomePage(){
+
+    return (
+
+        <>
+            <AppSection/>
+        </>
+
+    )
+
+}
+
+export default HomePage
