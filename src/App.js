c56d1399a657d96a4410cb5e86e0f9dcf2305c01
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-07-30 15:24:11 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './containers/HomePage/HomePage';
 
 function App() {
   return (
     <div>
-      <Router>
-        <Switch>
-          <Route path="/" component={HomePage} />
-        </Switch>
-      </Router>
+      <HomePage />
     </div>
   );
 }
