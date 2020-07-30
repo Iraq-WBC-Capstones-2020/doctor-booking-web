ec5edf876e9d5d9b8f1f61a724304e03d1144352
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-07-30 14:31:58 +0300

    Wrapped the OurServices section

---
+++ src/containers/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import OurServices from '../../components/OurServices/OurServices'
+
+function HomePage(){
+
+    return(
+        <>
+        <OurServices />
+        </>
+    )
+
+}
+
+export default HomePage
