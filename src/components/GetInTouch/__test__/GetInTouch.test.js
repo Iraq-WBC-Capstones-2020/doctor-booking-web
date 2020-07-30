0cfad39d419f9c49edb56909135ee6cf6ecadd32
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-07-30 21:52:41 +0300

    return back files

--- src/components/GetInTouch/__test__/GetInTouch.test.js
+++ src/components/GetInTouch/__test__/GetInTouch.test.js
@@ -3,11 +3,16 @@ import { render } from '@testing-library/react';
 import GetInTouch from '../GetInTouch';
 import renderer from 'react-test-renderer';
 
-test('GetInTouch is rendering ', () => {
-  render(<GetInTouch />);
-});
+// test('GetInTouch is rendering ', () => {
+//   render(<GetInTouch />);
+// });
 
-test('GetInTouch render correctly', () => {
-  const tree = renderer(<GetInTouch />).toJSON();
+// test('GetInTouch render correctly', () => {
+//   const tree = renderer(<GetInTouch />).toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
+
+it('GetInTouch render correctly', () => {
+  const tree = renderer.create(<GetInTouch />).toJSON();
   expect(tree).toMatchSnapshot();
 });
