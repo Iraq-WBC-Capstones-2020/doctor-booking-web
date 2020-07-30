84cbcdc651ff0f5eb60bf535c4ae8a9a584bb0a9
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-07-30 16:05:37 +0300

    added the HP to the app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Nav from './containers/Navbar/Navbar';
 import Footer from './containers/Footer/Footer';
+import HomePage from './containers/HomePage/HomePage'
 
 function App() {
   return (
     <div>
       <Nav />
+      <HomePage />
       <Footer />
     </div>
   );
