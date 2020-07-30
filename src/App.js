24d18fd7d45643a0812258f2dbd41f9f1fb6dac7
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-07-30 14:52:15 +0300

    make stand alone branch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import HomePage from './containers/HomePage/HomePage';
 import FaqPage from './containers/FaqPage/FaqPage';
 
 function App() {
   return (
     <div>
-      <Router>
-        <Switch>
-          <Route path="/faq" component={FaqPage} />
-          <Route path="/" component={HomePage} />
-        </Switch>
-      </Router>
+      <FaqPage />
     </div>
   );
 }
