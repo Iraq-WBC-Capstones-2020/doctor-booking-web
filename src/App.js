ab62d4a8e6da7ffb2ce5f62b03a4077da5feca7a
Hussein Talal <hossientalal2@gmail.com>
2020-07-31 20:41:15 +0300

    adding our team section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Nav from './containers/Navbar/Navbar';
 import Footer from './containers/Footer/Footer';
+import OurTeam from './components/AboutPage/OurTeam';
 
 function App() {
   return (
     <div>
       <Nav />
+      <OurTeam />
       <Footer />
     </div>
   );
