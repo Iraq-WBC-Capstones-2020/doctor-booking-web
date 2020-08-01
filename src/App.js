63267f59820ccba88b4f71c2fae300d033d9f407
Hussein Talal <hossientalal2@gmail.com>
2020-08-01 21:26:32 +0300

    installing react-router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './containers/HomePage/HomePage';
 import Nav from './containers/Navbar/Navbar';
 import Footer from './containers/Footer/Footer';
-
+import { Switch, Route } from 'react-router-dom';
+import AboutPage from './containers/AboutPage/About';
 function App() {
   return (
     <div>
       <Nav />
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/AboutPage" component={AboutPage} />
+      </Switch>
       <Footer />
     </div>
   );
