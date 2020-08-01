821d9600517f5ab5ae7221162ae42da4478cdf3c
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-08-01 14:08:55 +0300

    Run lint  and fix

--- src/components/AppSection/AppSection.js
+++ src/components/AppSection/AppSection.js
@@ -1,42 +1,40 @@
-import React from 'react'
-import {Row, Col, Container} from 'react-bootstrap'
-import appImg from './images/undraw_posting_photo_v65l.png'
-import appStore from './images/App Store - eng.png'
-import googlePlay from './images/Google Play - eng.png'
-import style from './style.css'
+import React from 'react';
+import { Row,  Container } from 'react-bootstrap';
+import appImg from './images/undraw_posting_photo_v65l.png';
+import appStore from './images/App Store - eng.png';
+import googlePlay from './images/Google Play - eng.png';
+import './style.css';
 
-
-function AppSection () {
-
-    return(
-         <Container className="ApplicationSection">
-            <Row className="myRow">
-            <div className="col-xs-6">
-                <div className="text">
-                        <h2>
-                        Download our 
-mobile apps 
-(comming soon)
-                        </h2>
-                        <p>
-                        Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, 
-or administrative hassle) and securely
-                        </p>
-                    </div>
-                    <div className="download-buttons">
-                        <a><img alt="App store" src={appStore}/></a>
-                        <a><img alt="Google Play" src={googlePlay}/></a>
-                    </div>
-                </div>
-                <div className="col-xs-6">
-                    <div className="Application-Image">
-                    <img  alt="our app" src={appImg}/>
-                    </div>
-                </div>
-            </Row>
-            </Container>
-    )
-    
+function AppSection() {
+  return (
+    <Container className="ApplicationSection">
+      <Row className="myRow">
+        <div className="col-xs-6">
+          <div className="text">
+            <h2>Download our mobile apps (comming soon)</h2>
+            <p>
+              Our dedicated patient engagement app and web portal allow you to
+              access information instantaneously (no tedeous form, long calls,
+              or administrative hassle) and securely
+            </p>
+          </div>
+          <div className="download-buttons">
+            <a>
+              <img alt="App store" src={appStore} />
+            </a>
+            <a>
+              <img alt="Google Play" src={googlePlay} />
+            </a>
+          </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="Application-Image">
+            <img alt="our app" src={appImg} />
+          </div>
+        </div>
+      </Row>
+    </Container>
+  );
 }
 
 export default AppSection;
--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,17 +1,14 @@
-
 import React from 'react';
 import IntroSection from '../../components/IntroSection/IntroSection';
 
-import AppSection from '../../components/AppSection/AppSection'
-
+import AppSection from '../../components/AppSection/AppSection';
 
 function HomePage() {
   return (
     <div>
       <IntroSection />
 
-      <AppSection/>
-
+      <AppSection />
     </div>
   );
 }
