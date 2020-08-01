08c9e4dc6ce104a9fda014d57dc7b1df48ad8912
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-08-01 16:35:16 +0300

    Added VideoSection to homePage

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import IntroSection from '../../components/IntroSection/IntroSection';
+import VideoSection from '../../components/VideoSection/VideoSection';
 
 function HomePage() {
   return (
     <div>
       <IntroSection />
+
+      <VideoSection />
     </div>
   );
 }
