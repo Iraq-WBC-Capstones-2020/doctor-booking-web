255ad09c3300cde98dbf4a63ed71044aa3af28a8
Hussein Talal <hossientalal2@gmail.com>
2020-08-02 01:16:11 +0300

    fixing nested links problem in navbar

--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -15,21 +15,21 @@ function nav() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto justify-content-end">
-            <Nav.Link>
-              <Link to="/">Home</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/SearchPage">Search</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/AboutPage">About</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/FAQPage">FAQ</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/SignInPage">Sign in</Link>
-            </Nav.Link>
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+            <Link className="nav-link" to="/SearchPage">
+              Search
+            </Link>
+            <Link className="nav-link" to="/AboutPage">
+              About
+            </Link>
+            <Link className="nav-link" to="/FAQPage">
+              FAQ
+            </Link>
+            <Link className="nav-link" to="/SignInPage">
+              Sign in
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
