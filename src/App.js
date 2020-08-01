2e1fad0487e78509083ff72958bba85b3457d8e9
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-08-01 13:58:56 +0300

    styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './containers/HomePage/HomePage';
 import Nav from './containers/Navbar/Navbar';
 import Footer from './containers/Footer/Footer';
-import HomePage from './containers/HomePage/HomePage'
 
 
 function App() {
