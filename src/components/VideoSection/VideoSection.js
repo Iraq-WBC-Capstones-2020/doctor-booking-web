a56e8a502932b7e7fba930e5b33e575b561c0b72
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-08-01 16:42:55 +0300

    run lint --fix

--- src/components/VideoSection/VideoSection.js
+++ src/components/VideoSection/VideoSection.js
@@ -1,33 +1,29 @@
-import React from 'react'
-import { Container, Row} from 'react-bootstrap'
-import videoFrame from './images/Video.png'
-import './style.css'
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+import videoFrame from './images/Video.png';
+import './style.css';
 
 function VideoSection() {
+  return (
+    <Container className="VideoSection">
+      <Row>
+        <div className="col-lg-6">
+          <h1>
+            To get more familiar <br />
+            with the website
+            <br />
+            watch this video
+            <br />
+          </h1>
+        </div>
 
-    return(
-
-        <Container className="VideoSection">
-            <Row>
-                <div className="col-lg-6">
-                
-                    <h1>To get more familiar <br/>
-                        with the website<br/>
-                        watch this video<br/>
-                    </h1>
-                </div>
-                
-           
-                <div className="col-lg-6">
-                    <div className="videoFrame">
-                    <img alt="Watch this video" src={videoFrame}/>
-                    </div>
-                </div>
-                
-            </Row>
-        </Container>
-
-    )
-    
+        <div className="col-lg-6">
+          <div className="videoFrame">
+            <img alt="Watch this video" src={videoFrame} />
+          </div>
+        </div>
+      </Row>
+    </Container>
+  );
 }
-export default VideoSection
+export default VideoSection;
