cb5c4f9688cc69306ce4ad9821445a068f3334b3
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-08-02 14:43:25 +0300

    create News letter section

---
+++ src/components/NewsLetter/NewsLetter.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Container, Col, Row, Form, InputGroup, Button,FormControl } from 'react-bootstrap'
+import ButtonImg from './images/Slice 1.png'
+function NewaLetter() {
+
+    return(
+        <Container className="NewsLetter">
+            <div className="theHead">
+            <Row className="align-items-center justify-content-center">
+                <h1>Never miss an update</h1>
+            </Row>
+            <Row className="align-items-center justify-content-center">
+                <p>Signup to get any update and serves we add </p>
+            </Row>
+            </div>
+            <div className="Email">
+            <Row className="align-items-center justify-content-center">
+                <Col sm={6}>
+            <InputGroup className="mb-3">
+    <FormControl className="inputFiled"
+      placeholder="Recipient's username"
+      aria-label="Recipient's username"
+      aria-describedby="basic-addon2"
+    />
+    <InputGroup.Append>
+    <input type="image" src={ButtonImg} alt="Submit" width="48" height="48"/>
+    </InputGroup.Append>
+  </InputGroup>
+  </Col>
+            </Row>
+            </div>
+        </Container>
+    )
+    
+}
+
+export default NewaLetter
