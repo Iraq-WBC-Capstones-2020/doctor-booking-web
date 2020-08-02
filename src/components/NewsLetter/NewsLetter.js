61b282d393294861933dad28c421f56e117df98b
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-08-02 15:29:46 +0300

    run lint and fix it

--- src/components/NewsLetter/NewsLetter.js
+++ src/components/NewsLetter/NewsLetter.js
@@ -1,38 +1,51 @@
-import React from 'react'
-import { Container, Col, Row, Form, InputGroup, Button,FormControl } from 'react-bootstrap'
-import ButtonImg from './images/Slice 1.png'
-import './style.css'
+import React from 'react';
+import {
+  Container,
+  Col,
+  Row,
+  Form,
+  InputGroup,
+  Button,
+  FormControl,
+} from 'react-bootstrap';
+import ButtonImg from './images/Slice 1.png';
+import './style.css';
 function NewsLetter() {
-
-    return(
-        <Container className="NewsLetter">
-            <div className="theHead">
-            <Row className="align-items-center justify-content-center">
-                <h1>Never miss an update</h1>
-            </Row>
-            <Row className="align-items-center justify-content-center">
-                <p>Signup to get any update and serves we add </p>
-            </Row>
-            </div>
-            <div className="Email">
-            <Row className="align-items-center justify-content-center">
-                <Col sm={4}>
+  return (
+    <Container className="NewsLetter">
+      <div className="theHead">
+        <Row className="align-items-center justify-content-center">
+          <h1>Never miss an update</h1>
+        </Row>
+        <Row className="align-items-center justify-content-center">
+          <p>Signup to get any update and serves we add </p>
+        </Row>
+      </div>
+      <div className="Email">
+        <Row className="align-items-center justify-content-center">
+          <Col sm={4}>
             <InputGroup className="mb-3">
-    <FormControl className="inputFiled"
-      placeholder="Recipient's username"
-      aria-label="Recipient's username"
-      aria-describedby="basic-addon2"
-    />
-    <InputGroup.Append>
-    <input type="image" src={ButtonImg} alt="Submit" width="48" height="48"/>
-    </InputGroup.Append>
-  </InputGroup>
-  </Col>
-            </Row>
-            </div>
-        </Container>
-    )
-    
+              <FormControl
+                className="inputFiled"
+                placeholder="Recipient's username"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <InputGroup.Append>
+                <input
+                  type="image"
+                  src={ButtonImg}
+                  alt="Submit"
+                  width="48"
+                  height="48"
+                />
+              </InputGroup.Append>
+            </InputGroup>
+          </Col>
+        </Row>
+      </div>
+    </Container>
+  );
 }
 
-export default NewsLetter
+export default NewsLetter;
