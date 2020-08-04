937ebeaa034cfb0c4637b03f1e2469b390505eb0
Hussein Talal <hossientalal2@gmail.com>
2020-08-04 13:43:13 +0300

    finalizing sign in page styles

--- src/containers/SignInPage/SignIn.js
+++ src/containers/SignInPage/SignIn.js
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 import SignInSection from '../../components/SignInPage/SignInSection';
 function SignIn() {
   return (
-    <Container className="d-flex justify-content-center mt-5">
+    <Container className="d-flex justify-content-center page-container">
       <SignInSection />
     </Container>
   );
