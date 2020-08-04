f402c2e9b86261d502e629934e019c951d81d397
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-08-04 16:48:16 +0300

    sorted the compoenys

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import OurServices from '../../components/OurServices/OurServices';
-//import IntroSection from '../../components/IntroSection/IntroSection';
+import IntroSection from '../../components/IntroSection/IntroSection';
 import GetInTouch from '../../components/GetInTouch/GetInTouch';
 import AppSection from '../../components/AppSection/AppSection'
 
 function HomePage() {
   return (
     <div>
+      <IntroSection />
       <GetInTouch />
-      <AppSection />
       <OurServices />
-
+      <AppSection />
     </div>
   );
 }
