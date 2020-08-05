31ec429d876306d873061e861e5914cf7d1b51d8
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-08-05 12:43:53 +0300

    delete footer

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from './containers/Navbar/Navbar';
 import HomePage from './containers/HomePage/HomePage';
-import Footer from './containers/Footer/Footer';
 
 function App() {
   return (
     <div>
       <Nav />
       <HomePage />
-      <Footer />
     </div>
   );
 }
