54dc35e77bb42ff86f3860d7dc61d103729d9ced
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-08-05 03:42:05 +0300

    lint and fix

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import OurServices from '../../components/OurServices/OurServices';
 import IntroSection from '../../components/IntroSection/IntroSection';
 import GetInTouch from '../../components/GetInTouch/GetInTouch';
-import NewsLetter from '../../components/NewsLetter/NewsLetter'
+import NewsLetter from '../../components/NewsLetter/NewsLetter';
 function HomePage() {
   return (
     <div>
