f12a84de5ba5bc77391814c5fc8d7b9514b13495
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-08-05 03:36:49 +0300

    resolve conflicts

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import OurServices from '../../components/OurServices/OurServices';
 import IntroSection from '../../components/IntroSection/IntroSection';
-<<<<<<< HEAD
-import NewsLetter from '../../components/NewsLetter/NewsLetter';
-=======
 import GetInTouch from '../../components/GetInTouch/GetInTouch';
->>>>>>> master
-
+import NewsLetter from '../../components/NewsLetter/NewsLetter'
 function HomePage() {
   return (
     <div>
       <IntroSection />
-<<<<<<< HEAD
-
-      <NewsLetter />
-=======
       <GetInTouch />
       <OurServices />
->>>>>>> master
+      <NewsLetter />
     </div>
   );
 }
