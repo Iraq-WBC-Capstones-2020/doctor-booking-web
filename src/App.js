a21c04a32ce49ea4c0e3b689980ea0e31da5dd86
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-08-06 16:29:28 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from './containers/Navbar/Navbar';
 import Footer from './containers/Footer/Footer';
-import BookingPage from './containers/BookingPage/BookingPage';
+import BookingPag e from './containers/BookingPage/BookingPage';
 
 function App() {
   return (
