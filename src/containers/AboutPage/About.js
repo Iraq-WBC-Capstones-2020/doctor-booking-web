0ed1d9bcb62e86a665f3a3bae1e4c5efe07e49f3
Mustafa Nawzat <mustafanawzatt@gmail.com>
2020-08-06 16:39:30 +0300

    include OurGoals components in About page

--- src/containers/AboutPage/About.js
+++ src/containers/AboutPage/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import AboutSection from '../../components/AboutSection/AboutSection';
+import OurGoals from '../../components/OurGoals/OurGoals';
 function About() {
   return (
     <>
       <AboutSection />
+      <OurGoals />
     </>
   );
 }
