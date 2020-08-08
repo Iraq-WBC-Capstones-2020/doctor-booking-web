92fb8dbe9405af1d8af5d00203b6415e4c0e7104
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-08-08 17:57:42 +0300

    fix conflect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from './containers/Navbar/Navbar';
+import HomePage from './containers/HomePage/HomePage';
 import Footer from './containers/Footer/Footer';
-import BookingPage from './containers/BookingPage/BookingPage';
 
 function App() {
   return (
     <div>
       <Nav />
-      <BookingPage />
+      <HomePage />
       <Footer />
     </div>
   );
