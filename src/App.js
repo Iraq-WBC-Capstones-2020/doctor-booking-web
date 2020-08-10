b9acae529b7364aa1caaa2f891be2ac6e341efcb
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-08-10 17:32:01 +0300

    delete usless things

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from './containers/Navbar/Navbar';
 import HomePage from './containers/HomePage/HomePage';
 import Footer from './containers/Footer/Footer';
-import DoctorRegistration from './containers/DoctorRegistration/DoctorRegistration';
 
 function App() {
   return (
     <div>
       <Nav />
       <HomePage />
-      <DoctorRegistration />
       <Footer />
     </div>
   );
