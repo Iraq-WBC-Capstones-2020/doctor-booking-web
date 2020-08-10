4409e7aba7e61fa4fe56c8ebbd2805be49c9594f
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-08-10 18:26:13 +0300

    fix and create the doctors obj

--- src/components/SearchPage/SearchResult/SearchResult.js
+++ src/components/SearchPage/SearchResult/SearchResult.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col, Button, Card } from 'react-bootstrap';
 import Avatar from './images/avatar.png';
 import './style.css';
 function SearchResult() {
+  const [card, setCard] = useState([
+    {
+      doctorName: 'Salah Mohammed',
+      specilty: 'Emergency medicine',
+      shortAbout:
+        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      id: 1,
+    },
+    {
+      doctorName: 'Salah Mohammed',
+      specilty: 'child Doc',
+      shortAbout:
+        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      id: 2,
+    },
+    {
+      doctorName: 'Sabah Mohammed',
+      specilty: 'Family medicine',
+      shortAbout:
+        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      id: 3,
+    },
+    {
+      doctorName: 'Salah Ahmed',
+      specilty: 'child Doc',
+      shortAbout:
+        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      id: 4,
+    },
+    {
+      doctorName: 'Salih Mohammed',
+      specilty: 'Medical genetics',
+      shortAbout:
+        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      id: 5,
+    },
+    {
+      doctorName: 'Salah Naji',
+      specilty: 'child Doc',
+      shortAbout:
+        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      id: 6,
+    },
+  ]);
+
   return (
-    <div className="SearchResult">
+    <div className="searchResult">
       <Container>
         <Row>
           <Col>
@@ -13,133 +58,32 @@ function SearchResult() {
 
           <hr className="solid" />
         </Row>
+
         <Row>
-          <Col md={4}>
-            <Card className="doctorCards" style={{ width: '18rem' }}>
-              <Card.Img className="myImgCard" variant="top" src={Avatar} />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <div className="specilty">specilty</div>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <div className="theButtons">
-                  <Button className="theWhite" variant="primary">
-                    More Info
-                  </Button>
-                  <Button className="theBlue" variant="primary">
-                    Book now
-                  </Button>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={4}>
-            <Card className="doctorCards" style={{ width: '18rem' }}>
-              <Card.Img className="myImgCard" variant="top" src={Avatar} />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <div className="specilty">specilty</div>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <div className="theButtons">
-                  <Button className="theWhite" variant="primary">
-                    More Info
-                  </Button>
-                  <Button className="theBlue" variant="primary">
-                    Book now
-                  </Button>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={4}>
-            <Card className="doctorCards" style={{ width: '18rem' }}>
-              <Card.Img className="myImgCard" variant="top" src={Avatar} />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <div className="specilty">specilty</div>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <div className="theButtons">
-                  <Button className="theWhite" variant="primary">
-                    More Info
-                  </Button>
-                  <Button className="theBlue" variant="primary">
-                    Book now
-                  </Button>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={4}>
-            <Card className="doctorCards" style={{ width: '18rem' }}>
-              <Card.Img className="myImgCard" variant="top" src={Avatar} />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <div className="specilty">specilty</div>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <div className="theButtons">
-                  <Button className="theWhite" variant="primary">
-                    More Info
-                  </Button>
-                  <Button className="theBlue" variant="primary">
-                    Book now
-                  </Button>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={4}>
-            <Card className="doctorCards" style={{ width: '18rem' }}>
-              <Card.Img className="myImgCard" variant="top" src={Avatar} />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <div className="specilty">specilty</div>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <div className="theButtons">
-                  <Button className="theWhite" variant="primary">
-                    More Info
-                  </Button>
-                  <Button className="theBlue" variant="primary">
-                    Book now
-                  </Button>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={4}>
-            <Card className="doctorCards" style={{ width: '18rem' }}>
-              <Card.Img className="myImgCard" variant="top" src={Avatar} />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <div className="specilty">specilty</div>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <div className="theButtons">
-                  <Button className="theWhite" variant="primary">
-                    More Info
-                  </Button>
-                  <Button className="theBlue" variant="primary">
-                    Book now
-                  </Button>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
+          {card.map((cards) => (
+            <Col md={4}>
+              <Card
+                key={cards.id}
+                className="doctorCards"
+                style={{ width: '18rem' }}
+              >
+                <Card.Img className="myImgCard" variant="top" src={Avatar} />
+                <Card.Body>
+                  <Card.Title>{cards.doctorName}</Card.Title>
+                  <div className="specilty">{cards.specilty}</div>
+                  <Card.Text>{cards.shortAbout}</Card.Text>
+                  <div className="theButtons">
+                    <Button className="theWhite" variant="primary">
+                      More Info
+                    </Button>
+                    <Button className="theBlue" variant="primary">
+                      Book now
+                    </Button>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
         </Row>
       </Container>
     </div>
