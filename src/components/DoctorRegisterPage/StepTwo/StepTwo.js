d7aa015f8b8e2f809dad479da65d0be66c3c503a
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-08-11 18:53:09 +0300

    setting and witting Step two

---
+++ src/components/DoctorRegisterPage/StepTwo/StepTwo.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import { Col, Form, Container, Row } from 'react-bootstrap'
+
+function StepTwo() {
+
+    const specilty = ["Abdominal radiology",
+         "Breast imaging",
+         "Cardiothoracic radiology",
+         "Cardiovascular radiology",
+         "Chest radiology",
+         "Emergency radiology",
+         "Endovascular surgical neuroradiology",
+         "Gastrointestinal radiology",
+         "Genitourinary radiology",
+         "Head and neck radiology",
+         "Pulmonary disease",
+            "Pulmonary disease", 
+            "Sleep medicine",
+            "Sports medicine",
+            "Transplant hepatology"
+        ];
+    
+return(
+
+    
+    <Container >
+        <Form>
+        <Row>
+            <Col lg={12}>
+        
+  <Form.Group controlId="exampleForm.SelectCustom">
+    <Form.Label>Speciality</Form.Label>
+    <Form.Control value="Speciality" as="select" custom>
+    {specilty.map(spec =>
+      <option>{spec}</option>
+     
+    )}
+    </Form.Control>
+  </Form.Group>
+
+</Col>
+        </Row>
+        <Row>
+        <Col lg={6}>
+            <Form.Group>
+    <label for="exampleFormControlTextarea1">Education</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              </Form.Group>
+                   
+            </Col>
+            <Col lg={6}>
+            <Form.Group>
+    <label for="exampleFormControlTextarea1">Expeience</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              </Form.Group>
+                   
+            </Col>
+        </Row>
+        </Form>
+    </Container>
+
+)
+
+}
+
+export default StepTwo
