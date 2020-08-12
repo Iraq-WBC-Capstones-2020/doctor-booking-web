7b98bb7e3222afe9e50b7ba989fe8916f3455c2b
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2020-08-12 12:24:53 +0300

    adding the calendar

--- src/components/DashboardPage/DashboardSection/DashboardSection.js
+++ src/components/DashboardPage/DashboardSection/DashboardSection.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Nav, Container } from 'react-bootstrap';
 import AppointmentCard from '../AppointmentCard/AppointmentCard';
+import Flatpickr from 'react-flatpickr';
 
 function DashboardSection() {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const days = document.getElementsByClassName('flatpickr-day');
+    console.log(days[0]);
+    for (let i = 0; i < days.length; i++) {
+      days[i].addEventListener('click', () => {
+        days[i].classList.add('selected');
+      });
+    }
+  }, []);
+
   return (
     <div className="container-fluid">
       <Row>
@@ -18,7 +31,7 @@ function DashboardSection() {
         </div>
         <Container className="main-content">
           <div className="date-container mt-5 mx-5">
-            <h2 className="mb-5">18 March 2020</h2>
+            <h2 className="mb-5">{date.toDateString()}</h2>
             <div id="line"></div>
           </div>
           <div className="appointments mt-5">
@@ -26,7 +39,17 @@ function DashboardSection() {
             <AppointmentCard />
           </div>
         </Container>
-        <div className="calendar"></div>
+        <div className="calendar-section d-flex justify-content-center">
+          <Flatpickr
+            className="calendar"
+            options={{
+              enableTime: false,
+              inline: true,
+              monthSelectorType: 'static',
+            }}
+            onChange={(date) => setDate(new Date(date[0]))}
+          />
+        </div>
       </Row>
     </div>
   );
