902d4b576c71c4b4a7873f9fac644f1c28a76fac
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-08-12 17:00:07 +0300

    Edit Profile Page - without Functionality Fixes #46 FINISH

---
+++ src/tests/EditProfile.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import EditProfile from '../components/EditProfile/EditProfile';
+
+test('EditProfile page run well run well ', () => {
+  const tree = renderer.create(<EditProfile />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
