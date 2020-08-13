995f39191f50db183df090f9a77eaff090a09e67
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-08-13 18:58:04 +0300

    styling ProfileHeader

---
+++ src/components/DoctorProfilePage/ProfileHeader/ProfileHeader.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Avatar from '../../AboutPage/imgs/avatar.svg';
+import { Container, Button } from 'react-bootstrap';
+import './style.css'
+
+
+function ProfileHeader(){
+
+    return(
+
+        <div className="cover container-fluid d-flex align-items-end">
+            <Container className="profileCover d-flex align-items-center">
+                <div className="profileImage">
+                    <img src={Avatar} alt="profile" />
+                </div>
+                <div className="profileName d-flex flex-column ml-4">
+                    <h2>
+                        Doctor Name
+                    </h2>
+                    <Button>
+                        Book an appointment
+                    </Button>
+                </div>
+               
+            </Container>
+        </div>
+
+    )
+
+}
+
+export default ProfileHeader
