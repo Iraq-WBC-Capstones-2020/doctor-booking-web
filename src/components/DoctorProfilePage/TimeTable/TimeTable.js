fa1dd322cf616229e60a89381f106109f8a15fea
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2020-08-13 19:02:41 +0300

    set time table files

---
+++ src/components/DoctorProfilePage/TimeTable/TimeTable.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, Col, Row } from 'react-bootstrap';
+
+function TimeTable(){
+
+    return(
+
+        <Container className="timeTable">
+
+            <Row>
+                <Col sm>
+                <div>dd</div>
+                </Col>
+            </Row>
+
+        </Container>
+
+    )
+    
+}
+
+export default TimeTable;
