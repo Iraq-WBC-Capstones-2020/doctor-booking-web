d2cd92eb249436196e45fd0b4f07e54660ecd245
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2020-08-16 11:52:18 +0300

    adding active link feature and changing height and font size

--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -3,58 +3,62 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 function nav() {
   return (
-    <Navbar
-      className="shadow-sm"
-      collapseOnSelect
-      expand="lg"
-      bg="white"
-      variant="light"
-    >
-      <Container>
-        <Navbar.Brand>Doctor Booking</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ml-auto justify-content-end">
-            <NavLink
-              activeClassName="active-link"
-              className="nav-link"
-              exact
-              to="/"
-            >
-              Home
-            </NavLink>
-            <NavLink
-              activeClassName="active-link"
-              className="nav-link"
-              to="/search"
-            >
-              Search
-            </NavLink>
-            <NavLink
-              activeClassName="active-link"
-              className="nav-link"
-              to="/about"
-            >
-              About
-            </NavLink>
-            <NavLink
-              activeClassName="active-link"
-              className="nav-link"
-              to="/faq"
-            >
-              FAQ
-            </NavLink>
-            <NavLink
-              activeClassName="active-link"
-              className="nav-link"
-              to="/signin"
-            >
-              Sign in
-            </NavLink>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div>
+      <Navbar
+        className="shadow-sm"
+        collapseOnSelect
+        expand="lg"
+        bg="white"
+        variant="light"
+      >
+        <Container>
+          <NavLink to="/" exact>
+            <Navbar.Brand>Doctor Booking</Navbar.Brand>
+          </NavLink>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto justify-content-end">
+              <NavLink
+                activeClassName="active-link"
+                className="nav-link"
+                exact
+                to="/"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                activeClassName="active-link"
+                className="nav-link"
+                to="/search"
+              >
+                Search
+              </NavLink>
+              <NavLink
+                activeClassName="active-link"
+                className="nav-link"
+                to="/about"
+              >
+                About
+              </NavLink>
+              <NavLink
+                activeClassName="active-link"
+                className="nav-link"
+                to="/faq"
+              >
+                FAQ
+              </NavLink>
+              <NavLink
+                activeClassName="active-link"
+                className="nav-link"
+                to="/signin"
+              >
+                Sign in
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 }
 
