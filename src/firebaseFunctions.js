d15afd4b8f64f32a9476956993a46a89b60e8bd2
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2020-08-18 23:32:46 +0300

    adding comment to clarify the code

--- src/firebaseFunctions.js
+++ src/firebaseFunctions.js
@@ -1,7 +1,8 @@
 import { db, auth } from './Firebase';
 
 export const firebaseFunctions = {
-  //put your function reference here
+  //put your function reference here like:
+  //functionName : function reference
 };
 
 //write your function here
