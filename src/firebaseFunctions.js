8c7355c5190cc56ef979fa358f306426474ba457
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2020-08-20 11:57:40 +0300

    adding signUp function to create the user and write user data in the database

--- src/firebaseFunctions.js
+++ src/firebaseFunctions.js
@@ -1,8 +1,40 @@
 import { db, auth } from './Firebase';
+import { firestore } from 'firebase';
 
 export const firebaseFunctions = {
   //put your function reference here like:
   //functionName : function reference
+  signUp: signUp,
 };
 
 //write your function here
+async function signUp(doctorInfo) {
+  let user;
+  await auth
+    .createUserWithEmailAndPassword(doctorInfo.email, doctorInfo.password)
+    .then((res) => (user = res.user))
+    .catch((err) => console.log(err));
+
+  const doctorData = {
+    name: doctorInfo.name,
+    province: doctorInfo.province,
+    city: doctorInfo.city,
+    reference: doctorInfo.reference,
+    neighborhood: doctorInfo.neighborhood,
+    location: new firestore.GeoPoint(
+      doctorInfo.marker[0],
+      doctorInfo.marker[1]
+    ),
+    phoneNumber: doctorInfo.phoneNumber,
+    gender: doctorInfo.gender,
+    speciality: doctorInfo.speciality,
+    education: doctorInfo.education,
+    experience: doctorInfo.experience,
+    timeTable: doctorInfo.timeTable,
+  };
+
+  db.collection('doctors')
+    .doc(user.uid)
+    .set(doctorData, { merge: true })
+    .then(() => console.log('document created and written'));
+}
