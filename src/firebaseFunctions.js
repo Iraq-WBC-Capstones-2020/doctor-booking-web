1c1a11765d9774df3f168b1c1c0b0da5f7bcc91e
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2020-08-21 16:11:40 +0300

    adding signIn function

--- src/firebaseFunctions.js
+++ src/firebaseFunctions.js
@@ -3,6 +3,17 @@ import { db, auth } from './Firebase';
 export const firebaseFunctions = {
   //put your function reference here like:
   //functionName : function reference
+  signIn: signIn,
 };
 
 //write your function here
+async function signIn(email, password) {
+  await auth
+    .signInWithEmailAndPassword(email, password)
+    .then((res) => {
+      console.log(res.user);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
