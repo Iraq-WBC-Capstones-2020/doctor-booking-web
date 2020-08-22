e7f52f4f7b4c9be0da85ddcd05eee93be446161b
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2020-08-22 19:34:16 +0300

    adding firebase config object properties

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,19 +1,19 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MESUREMENT_ID,
+  apiKey: 'AIzaSyDFOSXyLiKe02mshSlwYiYKOQpnjWaW2NU',
+  authDomain: 'doctor-booking-81f28.firebaseapp.com',
+  databaseURL: 'https://doctor-booking-81f28.firebaseio.com',
+  projectId: 'doctor-booking-81f28',
+  storageBucket: 'doctor-booking-81f28.appspot.com',
+  messagingSenderId: '1001900535989',
+  appId: '1:1001900535989:web:6139be47126e7bb432cefc',
+  measurementId: 'G-NR1Z21T4K4',
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+//firebase.analytics();
 
 export const db = firebase.firestore();
 export const auth = firebase.auth();
