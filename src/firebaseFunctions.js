315d1283bf9e35768029552e00f4b906760d43e5
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2020-08-22 19:35:14 +0300

    adding bookAppointment function to store the data in firestore

--- src/firebaseFunctions.js
+++ src/firebaseFunctions.js
@@ -3,6 +3,13 @@ import { db, auth } from './Firebase';
 export const firebaseFunctions = {
   //put your function reference here like:
   //functionName : function reference
+  bookAppointment: bookAppointment,
 };
 
 //write your function here
+function bookAppointment(appointmentInfo) {
+  return db
+    .collection('appointments')
+    .doc()
+    .set(appointmentInfo, { merge: true });
+}
