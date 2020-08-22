b967d2c4e2028189a5ddde7cfa892c1f0cf44f3f
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2020-08-22 11:34:07 +0300

    fixing test problems with useContext

--- src/Firebase.js
+++ src/Firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+//firebase.analytics();
 
 export const db = firebase.firestore();
 export const auth = firebase.auth();
--- src/tests/SignInPage.test.js
+++ src/tests/SignInPage.test.js
@@ -2,19 +2,33 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import SignInSection from '../components/SignInPage/SignInSection';
 import renderer from 'react-test-renderer';
-import SignIn from '../containers/SignInPage/SignIn';
+import DoctorInfoProvider from '../DoctorContext';
+
 test('sign in section is rendering ', () => {
-  render(<SignInSection />);
+  render(
+    <DoctorInfoProvider>
+      <SignInSection />
+    </DoctorInfoProvider>
+  );
 });
 
 test('sign in page renders correctly', () => {
-  const tree = renderer.create(<SignIn />).toJSON();
+  const tree = renderer
+    .create(
+      <DoctorInfoProvider>
+        <SignInSection />
+      </DoctorInfoProvider>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test('e-mail should be string', () => {
-  const signIn = render(<SignInSection />);
-  console.log(signIn);
+  const signIn = render(
+    <DoctorInfoProvider>
+      <SignInSection />
+    </DoctorInfoProvider>
+  );
   const input = signIn.getByPlaceholderText('E-mail');
   fireEvent.change(input, { target: { value: 'anything@anything.com' } });
   expect(input.value).toBe('anything@anything.com');
