efe89a5882b89706416d326aa85642860050292e
Hussein Talal <63732507+husseinTalal2@users.noreply.github.com>
2020-08-24 17:50:05 +0300

    fixing tests problems

--- src/tests/Navbar.test.js
+++ src/tests/Navbar.test.js
@@ -3,20 +3,25 @@ import { render } from '@testing-library/react';
 import Navbar from '../containers/Navbar/Navbar';
 import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
+import DoctorInfoProvider from '../DoctorContext';
 test('navbar is rendering ', () => {
   render(
-    <MemoryRouter>
-      <Navbar />
-    </MemoryRouter>
+    <DoctorInfoProvider>
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>
+    </DoctorInfoProvider>
   );
 });
 
 test('navbar renders correctly', () => {
   const tree = renderer
     .create(
-      <MemoryRouter>
-        <Navbar />
-      </MemoryRouter>
+      <DoctorInfoProvider>
+        <MemoryRouter>
+          <Navbar />
+        </MemoryRouter>
+      </DoctorInfoProvider>
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
