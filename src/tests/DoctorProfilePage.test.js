ba56a321657c83589dc13787455ac3f0f1d4d2c7
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-08-26 21:38:51 +0300

    fix test

--- src/tests/DoctorProfilePage.test.js
+++ src/tests/DoctorProfilePage.test.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import DoctorProfilePage from '../containers/DoctorProfilePage/DoctorProfilePage';
-
-// test('DoctorProfilePage  is rendering  ', () => {
-//   render(<DoctorProfilePage />);
-// });
 
 test('DoctorProfilePage run well ', () => {
   const tree = renderer.create(<DoctorProfilePage />).toJSON();
